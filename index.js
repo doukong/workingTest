aef36fb08a3e8026faf9077652446b5e7c4278cb
임동완 <limson_95@naver.com>
2019-10-24 16:44:40 +0900

    save visit register and res stats

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const randomstring = require('randomstring')
 const fs = require('fs')
+const moment = require('moment-timezone')
 const registerJson = require('./register.json')
 
 const app = express()
@@ -29,9 +30,34 @@ function findId(id){
 
 function makeId(findUrl){
     urlId = randomstring.generate(7)
-    registerJson.push({url:findUrl,id:urlId})
-    // let writeJson = JSON.stringify(registerJson)
+    registerJson.push({url:findUrl,id:urlId,stats:[]})
+    saveStat(urlId)
     return urlId
+}
+
+function saveStat(id){
+    const nowDate = moment.tz(new Date(),"Asia/Seoul").format('YYYY-MM-DD HH:00:00')
+    const indexNum = findStat(nowDate,id)
+    if(indexNum[1] == -1){
+        registerJson[indexNum[0]].stats.push({at:nowDate,visit:1})
+    }else{
+        registerJson[indexNum[0]].stats[indexNum[1]].visit += 1
+    }
+}
+
+function findIdIndex(id){
+    const indexNum = registerJson.findIndex((data,index)=>{
+        return data.id == id
+    })
+    return indexNum
+}
+
+function findStat(date,id){
+    const indexNum = findIdIndex(id)
+    const statsIndexNum = registerJson[indexNum].stats.findIndex((data,index)=>{
+        return data.at == date
+    })
+    return [indexNum,statsIndexNum]
 }
 
 
@@ -40,13 +66,18 @@ app.post("/register.json",(req,res)=>{
     if(id == "not"){
         res.status(201).contentType('application/json').json({Body:{ url: `http://localhost:3000/${makeId(req.query.url)}`}})
     }else{
+        saveStat(id)
         res.status(200).contentType('application/json').json({Body: { url: `http://localhost:3000/${id}`} })
     }
 })
 
 app.get("/:id",(req,res)=>{
     const originalUrl = findId(req.params.id)
-    console.log(originalUrl)
+    res.status(301).contentType('application/json').json({Location:originalUrl})
+})
+app.get("/:id/stats",(req,res)=>{
+    const indexNum = findIdIndex(req.params.id)
+    res.status(200).contentType('application/json').json({Body:{"Stats":registerJson[indexNum].stats}})
 })
 
 app.listen(port,()=>console.log("work"))
