a84f1eb4ac0a18f4e9d1e5f148e910004c25a2df
임동완 <limson_95@naver.com>
2019-10-24 15:36:02 +0900

    res have url id

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const randomstring = require('randomstring')
+const reigsterJson = require('./register')
 
 const app = express()
-const port = 3001
+const port = 3000
+
+function findUrl(url){
+    let id = "not"
+    reigsterJson.map(Data=>{
+        if(Data.url == url){
+            id = Data.id 
+        }
+    })
+    return id
+}
+function makeId(findUrl){
+    reigsterJson.push({url:findUrl,id:randomstring.generate(7)})
+    console.log(reigsterJson)
+}
+
+app.post("/register.json",(req,res)=>{
+    const id = findUrl(req.query.url)
+    if(id == "not"){
+        makeId(req.query.url)
+    }else{
+        res.status(200).contentType('application/json').json({Body: { url: `http://localhost:3000/${id}`} })
+    }
+})
 
 app.listen(port,()=>console.log("work"))
---
+++ register.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        "url":"hi",
+        "id":"asdf"
+    },
+    {
+        "url":"http://www.vingle.net",
+        "id":"whatthe"
+    }
+]
